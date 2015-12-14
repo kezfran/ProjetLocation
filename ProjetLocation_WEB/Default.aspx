4fde5d2599f563256d80192d04a45a25c38e4a8e
bisa12345 <200733652@B1180-POSTE09.Ahuntsic.ca.intra>
2015-12-14 15:45:00 -0500

    default
    default

---
+++ ProjetLocation_WEB/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ProjetLocation_WEB._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <form id="form1" runat="server">
+        <asp:Panel ID="Panel1" runat="server" Height="455px">
+            <p>Login</p>
+            <div style="padding: 10px;">
+                <p>ID: <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+                    <br />
+                    <asp:Label ID="IDRequis" runat="server" Text="*ID requis" ForeColor="Red" Font-Bold="True"></asp:Label>
+                </p>
+                <p>Password: <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+                    <br />
+                    <asp:Label ID="passRequis" runat="server" Text="*password requis" ForeColor="Red" Font-Bold="True"></asp:Label>
+                </p>
+                <p><asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" /></p>
+            </div>
+        </asp:Panel>
+    </form>
+</asp:Content>
---
+++ ProjetLocation_WEB/Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProjetLocation.db;
+using MySql.Data.MySqlClient;
+using ProjetLocation.service.implementations;
+using ProjetLocation.dto;
+
+namespace ProjetLocation_WEB
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserAuthentication"] != null)
+            {
+                Response.Redirect("~/Manage.aspx");
+            }
+            IDRequis.Visible = false;
+            passRequis.Visible = false;
+        }
+
+        private void VerificationLogin()
+        {
+            EmployeService employe = new EmployeService();
+            EmployeDTO emp = new EmployeDTO();
+
+            int id = Int32.Parse(txtID.Text);
+            string motPasse = txtPassword.Text;
+
+            try
+            {
+                //connexion.Open();
+                emp = employe.ReadEmploye(id);
+
+                if (txtID.Equals(null))
+                {
+                    IDRequis.Visible = true;
+                }else if(motPasse.Equals(null)){
+                    passRequis.Visible = true;
+                }
+                else if (emp.IdEmploye != id || emp.MotPasse != motPasse)
+                {
+                    IDRequis.Visible = false;
+                    passRequis.Visible = false;
+                    MessageBox.Show("Votre ID ou votre mot de passe est invalide!");
+                    // INVALID PASS
+                }
+                else
+                {
+                    // SUCCESSFUL LOGIN
+                    Session["UserAuthentication"] = emp.IdEmploye;
+                    Response.Redirect("Manage.aspx");
+                }
+
+            }
+            catch (MySqlException mySqlException)
+            {
+                throw new Exception(mySqlException.Message);
+            }
+            //finally 
+            //{ 
+            //    //connexion.Close();
+            //}
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            VerificationLogin();
+        }
+    }
+}
