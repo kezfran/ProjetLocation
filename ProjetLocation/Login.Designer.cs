5a8602809718fed8780caa72c807f34b9a00d85f
franz <kezfranz@yahoo.fr>
2015-12-15 09:49:05 -0500

    modif login

--- ProjetLocation/Login.Designer.cs
+++ ProjetLocation/Login.Designer.cs
@@ -41,41 +41,46 @@
             // lblID
             // 
             this.lblID.AutoSize = true;
-            this.lblID.Location = new System.Drawing.Point(147, 66);
+            this.lblID.Location = new System.Drawing.Point(287, 179);
+            this.lblID.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblID.Name = "lblID";
-            this.lblID.Size = new System.Drawing.Size(24, 13);
+            this.lblID.Size = new System.Drawing.Size(42, 25);
             this.lblID.TabIndex = 0;
             this.lblID.Text = "ID :";
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(112, 106);
+            this.lblPassword.Location = new System.Drawing.Point(218, 255);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(59, 13);
+            this.lblPassword.Size = new System.Drawing.Size(109, 25);
             this.lblPassword.TabIndex = 1;
             this.lblPassword.Text = "Password :";
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(171, 63);
+            this.txtID.Location = new System.Drawing.Point(335, 175);
+            this.txtID.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.Size = new System.Drawing.Size(196, 30);
             this.txtID.TabIndex = 2;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(171, 103);
+            this.txtPassword.Location = new System.Drawing.Point(335, 252);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.Size = new System.Drawing.Size(196, 30);
             this.txtPassword.TabIndex = 3;
             this.txtPassword.UseSystemPasswordChar = true;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(115, 154);
+            this.btnLogin.Location = new System.Drawing.Point(223, 351);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.Size = new System.Drawing.Size(149, 45);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -83,9 +88,10 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(196, 154);
+            this.btnExit.Location = new System.Drawing.Point(386, 351);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(149, 45);
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -94,31 +100,33 @@
             // IDRequis
             // 
             this.IDRequis.AutoSize = true;
-            this.IDRequis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDRequis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.IDRequis.ForeColor = System.Drawing.Color.Red;
-            this.IDRequis.Location = new System.Drawing.Point(169, 86);
+            this.IDRequis.Location = new System.Drawing.Point(331, 218);
+            this.IDRequis.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.IDRequis.Name = "IDRequis";
-            this.IDRequis.Size = new System.Drawing.Size(61, 15);
+            this.IDRequis.Size = new System.Drawing.Size(69, 17);
             this.IDRequis.TabIndex = 6;
             this.IDRequis.Text = "*ID requis";
             // 
             // passRequis
             // 
             this.passRequis.AutoSize = true;
-            this.passRequis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passRequis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.passRequis.ForeColor = System.Drawing.Color.Red;
-            this.passRequis.Location = new System.Drawing.Point(169, 126);
+            this.passRequis.Location = new System.Drawing.Point(331, 294);
+            this.passRequis.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.passRequis.Name = "passRequis";
-            this.passRequis.Size = new System.Drawing.Size(102, 15);
+            this.passRequis.Size = new System.Drawing.Size(116, 17);
             this.passRequis.TabIndex = 7;
             this.passRequis.Text = "*password requis";
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(415, 233);
+            this.ClientSize = new System.Drawing.Size(829, 449);
             this.Controls.Add(this.passRequis);
             this.Controls.Add(this.IDRequis);
             this.Controls.Add(this.btnExit);
@@ -127,6 +135,8 @@
             this.Controls.Add(this.txtID);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblID);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
